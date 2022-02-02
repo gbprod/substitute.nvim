1af4d899c05a8549e4e46c3f3b18333ff545c0d6
gbprod <contact@gb-prod.fr>
2022-02-02 13:33:42 +0100

    feat(range): introduce motion1 and motion2 options
    Ref: #4

--- lua/substitute/config.lua
+++ lua/substitute/config.lua
@@ -11,6 +11,8 @@ local function with_defaults(options)
       prompt_current_text = options.range and options.range.prompt_current_text or false,
       confirm = options.range and options.range.confirm or false,
       complete_word = options.range and options.range.complete_word or false,
+      motion1 = options.range and options.range.motion1 or false,
+      motion2 = options.range and options.range.motion2 or false,
     },
   }
 end
@@ -24,7 +26,9 @@ function config.get_range(overrides)
     prefix = overrides.prefix or config.options.range.prefix,
     prompt_current_text = overrides.prompt_current_text or config.options.range.prompt_current_text,
     confirm = overrides.confirm or config.options.range.confirm,
-    complete_word = overrides.complete_word or config.options.complete_word,
+    complete_word = overrides.complete_word or config.options.range.complete_word,
+    motion1 = overrides.motion1 or config.options.range.motion1,
+    motion2 = overrides.motion2 or config.options.range.motion2,
   }
 end
 
